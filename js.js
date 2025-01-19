5ec731487f85f660fe3cfac6f4533fdaf668b542
Shokrullah Kosha <130617495+Koshaweb@users.noreply.github.com>
2025-01-20 08:52:42 +1100

    Initial commit

---
+++ js.js
@@ -0,0 +1,105 @@
+callId = ($id) => {
+ return document.getElementById($id);
+}
+
+
+const startBtn = callId('start-btn');
+const WelScreen = callId('welcome-screen');
+const QzScreen = callId('quiz-screen');
+const RstScreen = callId('result-screen');
+const RstBtn = callId('restart-btn');
+const QuestionNumber = callId('question-number');
+const questionTitle = callId('question-title');
+const options = callId('options');
+const scoreDisplay = callId('score');
+
+
+let questionsData = [
+  {
+    question: "What is the capital of Australia?",
+    options: ["Melbourne", "Sydney", "Adelaide", "Canberra"],
+    correct: "Canberra",
+  },
+
+  {
+    question: "Who is the the author of Becoming?",
+    options: ["Obama", "Micheal Obama", "Brian Tracy", "Shokrullah Kosha"],
+    correct: "Micheal Obama",
+  },
+
+  {
+    question: "How many people were killed in Saudi Arabia accident on 2015 at Kaba?",
+    options: ["2100", "3500", "805", "1200"],
+    correct: "805",
+  },
+
+  {
+    question: "How states does Australia have?",
+    options: ["6", "2", "8", "5"],
+    correct: "6",
+  },
+
+  {
+    question: "Who is the author of Fear Money Purpose?",
+    options: ["Nancy Youssef", "Shokrullah Kosha", "Brian Tracy", "Yuwal Noh Herari"],
+    correct: "Nancy Youssef",
+  },
+
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+startQuiz = () =>{
+  WelScreen.classList.add("hidden");
+  QzScreen.classList.remove("hidden");
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion ();
+}
+
+showQuestion = () => {
+  const currentQuestion = questionsData[currentQuestionIndex];
+  QuestionNumber.textContent = `${currentQuestionIndex + 1} of ${questionsData.length}`;
+  questionTitle.textContent = currentQuestion.question;
+  options.innerHTML = "";
+
+  currentQuestion.options.forEach((option) =>{
+    const button = document.createElement("button");
+    button.textContent = option;
+    button.className ="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg transition-all";
+    button.onclick = () => checkAnswer(option);
+    options.appendChild(button);
+  })
+};
+
+checkAnswer = (selectedOption) => {
+  const currentQuestion = questionsData[currentQuestionIndex];
+
+  if(selectedOption === currentQuestion.correct){
+    score++;
+  } 
+
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questionsData.length){
+    showQuestion();
+  }else{
+    showResult();
+  }
+};
+
+
+showResult = () => {
+  QzScreen.classList.add("hidden");
+  RstScreen.classList.remove("hidden");
+
+  scoreDisplay.textContent = `your Score is ${score} out of ${questionsData.length}`;
+}
+
+RstBtn.addEventListener("click", () => {
+    RstScreen.classList.add("hidden");
+    WelScreen.classList.remove("hidden");
+   });
+
+ startBtn.addEventListener("click", startQuiz);
